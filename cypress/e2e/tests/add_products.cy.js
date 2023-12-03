b0e0cf133b7ccb5936048073751120cda3d1ddef
Ana Gallardo <anagallardo03@gmail.com>
2023-12-03 22:41:13 +0000

    Adding comments

--- cypress/e2e/tests/add_products.cy.js
+++ cypress/e2e/tests/add_products.cy.js
@@ -9,6 +9,7 @@ describe('adding products to the cart', () => {
   const productElements = new ProductElements();
   const cartElements = new CartElements();
 
+  //Array with products and categories that will be added
   const products = [
     { name: 'Nokia lumia 1520', category: 'Phones' },
     { name: 'MacBook air', category: 'Laptops' },
@@ -18,12 +19,15 @@ describe('adding products to the cart', () => {
 
   it('should add products to the cart', () => {
     cy.visit(Cypress.config('base_url'));
+
+    //This is executed for all the products in the array
     for (const product of products) {
       homeElements.categoryOption(product.category).click();
       homeElements.productOption(product.name).click();
       productElements.addProductBtn.click();
       productElements.popUp('Product added').should('be.visible');
 
+      //Checks if the product added exist in the cart list
       homeElements.cartLink.click();
       cartElements.productAdded(product.name).should('be.visible');
       cartElements.deleteOption(product.name).should('exist');
--- cypress/e2e/tests/purchase_products.cy.js
+++ cypress/e2e/tests/purchase_products.cy.js
@@ -14,6 +14,8 @@ describe('complete the purchase', () => {
     const paymentFormElements = new PaymentFormElements();
     const purchaseConrfimationElements = new PurchaseConrfimationElements();
 
+
+    //Array with products and categories that will be added
     const products = [
         { name: 'Nokia lumia 1520', category: 'Phones' },
         { name: 'MacBook air', category: 'Laptops' },
@@ -23,6 +25,8 @@ describe('complete the purchase', () => {
 
     it('should complete the purchase', () => {
         cy.visit(Cypress.config('base_url'));
+
+        //This is executed for all the products in the array
         for (const product of products) {
             homeElements.categoryOption(product.category).click();
             homeElements.productOption(product.name).click();
@@ -35,9 +39,12 @@ describe('complete the purchase', () => {
             homeElements.homeLink.click();
         }
 
+        //Goes to the cart
         homeElements.cartLink.click();
         cartElements.totalToBePaid('1520').should('be.visible');
         cartElements.placeOrderBtn.click();
+
+        //Completing the payment data
         paymentFormElements.modal.should('be.visible');
         paymentFormElements.nameInput.click().invoke('val', faker.person.firstName);
         paymentFormElements.countryInput.click().invoke('val', faker.location.country);
@@ -45,6 +52,8 @@ describe('complete the purchase', () => {
         paymentFormElements.creditCardInput.click().invoke('val', faker.finance.creditCardNumber);
         paymentFormElements.monthInput.click().invoke('val', faker.number.int({ min: 1, max: 12 }));
         paymentFormElements.yearInput.click().invoke('val', faker.number.int({ min: 2025, max: 2035 }));
+
+        //Completing the purchase
         paymentFormElements.paymentBtn.click();
         purchaseConrfimationElements.modalTitle.should('be.visible');
         purchaseConrfimationElements.greenCheckMark.should('exist');
